188f47109147af6c50badba69358b0318aec60e6
Nuraiym Ishenbekova <ishenbekova_n@auca.kg>
2022-09-14 15:08:41 +0600

    Finish Problem  3 in Lab 1

--- lab-01/src/Problem03.java
+++ lab-01/src/Problem03.java
@@ -4,7 +4,8 @@ public class Problem03 {
     public static void main(String[] args) {
         System.out.println("What is your name?");
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();
-        System.out.println("Hello " + name + "!");
+        String firstName;
+        firstName = scanner.next();
+        System.out.println("Hello " + firstName + "!");
     }
 }

3134c08b6c6eef84db3ae26218f792164f2e185d
Nuraiym Ishenbekova <ishenbekova_n@auca.kg>
2022-09-14 15:13:52 +0600

    Fixed Problem  3 in Lab 1

--- lab-01/src/Problem03.java
+++ lab-01/src/Problem03.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Problem03 {
     public static void main(String[] args) {
-        System.out.println("What is your name?");
+        System.out.print("What is your name? ");
         Scanner scanner = new Scanner(System.in);
         String firstName;
         firstName = scanner.next();
-        System.out.println("Hello " + firstName + "!");
+        System.out.println("Hello, " + firstName + "!");
     }
 }

d6ba8a1fc165d44da754b8cd88a40679baff8d80
Nuraiym Ishenbekova <ishenbekova_n@auca.kg>
2022-10-17 18:49:31 +0600

    OJ Midterm Problem 20 is fixed

--- oj-midterm/src/Problem20.java
+++ oj-midterm/src/Problem20.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Problem20 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         int phoneNumber = input.nextInt();
 
         int prefix = phoneNumber / 10000;

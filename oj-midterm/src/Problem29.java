3137845d1331f25111324fc11988b9874cfd13ac
Nuraiym Ishenbekova <ishenbekova_n@auca.kg>
2022-10-30 01:56:12 +0600

    OJ Midterm Problem 29 is accepted

---
+++ oj-midterm/src/Problem29.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Problem29 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int bowl1 = input.nextInt();
+        int bowl2 = input.nextInt();
+        int bowl3 = input.nextInt();
+
+        if (bowl1 > bowl2 && bowl1 < bowl3 || bowl1 > bowl3 && bowl1 < bowl2) {
+            System.out.println(bowl1);
+        } else if (bowl2 > bowl1 && bowl2 < bowl3 || bowl2 > bowl3 && bowl2 < bowl1 ) {
+            System.out.println(bowl2);
+        } else {
+            System.out.println(bowl3);
+        }
+    }
+}

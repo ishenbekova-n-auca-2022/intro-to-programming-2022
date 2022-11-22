69699fabd43c5bb6f3c3fd9797b321c6fc76b748
Nuraiym Ishenbekova <ishenbekova_n@auca.kg>
2022-11-22 13:18:21 +0600

    Problem 07 is completed

---
+++ lab-05/src/Problem07.java
@@ -0,0 +1,26 @@
+public class Problem07 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int number = 0;
+
+        //first case
+        while (number < 20) {
+            number ++;
+            sum += number;
+            if (sum >= 100)
+                break;
+        }
+
+        System.out.println("The number is " + number);
+        System.out.println("The sum is " + sum);
+
+        //second case
+        while (number < 20) {
+            number ++;
+            if (number == 10 || number == 11)
+                continue;
+            sum += number;
+        }
+        System.out.println("The sum is " + sum);
+    }
+}

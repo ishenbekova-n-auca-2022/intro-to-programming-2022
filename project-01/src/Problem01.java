3eb8a9b0291588eea6486d3c3fc0168983ce2435
Nuraiym Ishenbekova <ishenbekova_n@auca.kg>
2022-11-22 21:43:20 +0600

    Doing project-01. Setting colors

--- project-01/src/Problem01.java
+++ project-01/src/Problem01.java
@@ -2,6 +2,7 @@ import processing.core.PApplet;
 
 public class Problem01 extends PApplet {
     int x = 0, y = 0;
+    int dx = 15, dy = 15;
 
     public void settings() {
         fullScreen();
@@ -18,20 +19,30 @@ public class Problem01 extends PApplet {
 
         rect(0, 0, width, height);
 
-        fill(0, 130, 238);
+        fill(57, 130, 238);
         circle(width / 2f + x, height / 2f + y, 60);
 
-        fill(0, 0, 255);
+        fill(198, 45, 255);
         circle(width / 4f + x, height / 4f + y, 60);
 
         fill(135, 105, 63);
         circle(width / 4f + x, height / 2f + height / 4f + y, 60);
 
-        fill(0, 255, 0);
+        fill(72, 255, 90);
         circle(width / 4f + width / 2f + x, height / 4f + y, 60);
 
-        fill(255, 0, 0);
+        fill(146, 200, 20);
         circle(width / 4f + width / 2f + x, height / 4f + height / 2f + y, 60);
+
+        if (x >= width / 4 - 30 || x <= -width / 4 + 30) {
+            dx = -dx;
+        }
+        if (y >= height / 4 - 30 || y <= -height / 4 + 30) {
+            dy = -dy;
+        }
+
+        x += dx;
+        y += dy;
     }
 
     public static void main(String[] args) {

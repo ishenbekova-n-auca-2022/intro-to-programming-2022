258333be63ec94d1681fb59361a441dc397916de
Nuraiym Ishenbekova <ishenbekova_n@auca.kg>
2022-11-28 21:36:21 +0600

    Rectangle is drawn in Project03

--- project-01/src/Problem03.java
+++ project-01/src/Problem03.java
@@ -2,12 +2,21 @@ import processing.core.PApplet;
 
 public class Problem03 extends PApplet {
 
+    float rectX;
+    float rectY;
+    float rectWidth;
+    float rectHeight;
+
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
         noStroke();
+        rectWidth = width / 3f;
+        rectHeight = height / 3f;
+        rectX = width / 2f - rectWidth / 2f;
+        rectY = height / 2f - rectHeight / 2f;
     }
 
     public void draw() {
@@ -15,8 +24,10 @@ public class Problem03 extends PApplet {
 
         fill(0, 40);
         rect(0, 0, width, height);
+        fill(0,100, 100);
+        rect(rectX, rectY, rectWidth, rectHeight);
         fill(0, 0, 255);
-        circle(mouseX, mouseY, 30);
+        circle(mouseX, mouseY, 50);
     }
 
     public static void main(String[] args) {
